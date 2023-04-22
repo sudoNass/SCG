9156d64571f2a05f58e0b18943fb593aaf109550
sudoNass <nass.asmari@gmail.com>
2023-04-22 05:05:55 +0300

    lvl1 done

--- Level-1/hint.js
+++ Level-1/hint.js
@@ -1,5 +1,5 @@
 // Example of underflow vulnerability in JS
-var a = 10000000000000000 // 16 zeroes, try with 15 zeroes ;)
+var a = 1000000000000000 // 16 zeroes, try with 15 zeroes ;)
 var b = 2
 var c = 1
 
